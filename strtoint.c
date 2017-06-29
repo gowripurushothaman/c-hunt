f1c7bfd4671918439523dfd31e63e8e708005a51
gowripurushothaman <gowrikalpana27@gmail.com>
2017-06-29 20:37:20 +0530

    Create strtoint.c

---
+++ strtoint.c
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char nam[10];
+int dec=0,i,len;
+gets(num);
+len=strlen(num);
+for(i=0;i<num;i++)
+{
+dec=dec*10+(a[i]-'0');
+}
+printf("%d",dec);
+return 0;
+}
